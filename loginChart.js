2785719201f00df792e33b49c0717628d44eee15
shreeyt499-ship-it <shree.yt.499@gmail.com>
2026-03-11 00:06:43 +0530

    Delete loginChart.js

--- loginChart.js
+++
@@ -1,6 +0,0 @@
-const loginChartEl = document.getElementById("loginChart");
-if (loginChartEl) {
-    new Chart(loginChartEl, {
-        type: "line",
-        data: {
-            labels: ["Jan", "Feb
