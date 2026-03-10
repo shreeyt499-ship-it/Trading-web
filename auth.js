2959c8326617194b0e1127244d21d87e19e2250f
shreeyt499-ship-it <shree.yt.499@gmail.com>
2026-03-10 22:22:59 +0530

    Refactor login and signup functions to use imports

--- auth.js
+++ auth.js
@@ -1,33 +1,18 @@
-function login(){
+import { auth } from './firebase.js';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-const email=document.getElementById("email").value
-const password=document.getElementById("password").value
-
-auth.signInWithEmailAndPassword(email,password)
-
-.then(()=>{
-
-window.location="dashboard.html"
-
-})
-
-.catch(e=>alert(e.message))
-
+export function login(){
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => window.location = "dashboard.html")
+    .catch(e => alert(e.message));
 }
 
-function signup(){
-
-const email=document.getElementById("email").value
-const password=document.getElementById("password").value
-
-auth.createUserWithEmailAndPassword(email,password)
-
-.then(()=>{
-
-window.location="dashboard.html"
-
-})
-
-.catch(e=>alert(e.message))
-
+export function signup(){
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => window.location = "dashboard.html")
+    .catch(e => alert(e.message));
 }
