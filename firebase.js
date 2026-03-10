ae2ec346c7005631d0aa4650d4bfe42bc1bcc4f7
shreeyt499-ship-it <shree.yt.499@gmail.com>
2026-03-10 22:02:47 +0530

    Upgrade Firebase SDK to version 10.8.0

--- firebase.js
+++ firebase.js
@@ -1,9 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+// Your config
 const firebaseConfig = {
-  apiKey: "AIzaSyAQkrYNZ4uR_FIFV0zE7ioEetu7xb5bGlc" ,
+  apiKey: "AIzaSyAQkrYNZ4uR_FIFV0zE7ioEetu7xb5bGlc",
   authDomain: "trading-journal-1ea0f.firebaseapp.com",
   projectId: "trading-journal-1ea0f",
   storageBucket: "trading-journal-1ea0f.firebasestorage.app",
@@ -11,9 +13,10 @@ const firebaseConfig = {
   appId: "1:519108528223:web:259b83ec90548a2848b3ea"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+// Services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
