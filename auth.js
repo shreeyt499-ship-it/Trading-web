c22b81a1a4eb0a0deee7836fcbc9f7abea51d95d
shreeyt499-ship-it <shree.yt.499@gmail.com>
2026-03-10 22:29:04 +0530

    Refactor login and signup functions for clarity

--- auth.js
+++ auth.js
@@ -1,18 +1,31 @@
 import { auth } from './firebase.js';
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-export function login(){
+// LOGIN
+window.login = function(){
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+
   signInWithEmailAndPassword(auth, email, password)
-    .then(() => window.location = "dashboard.html")
-    .catch(e => alert(e.message));
+  .then(()=>{
+    window.location = "dashboard.html";
+  })
+  .catch(e => alert(e.message));
+
 }
 
-export function signup(){
+// SIGNUP
+window.signup = function(){
+
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+
   createUserWithEmailAndPassword(auth, email, password)
-    .then(() => window.location = "dashboard.html")
-    .catch(e => alert(e.message));
+  .then(()=>{
+    window.location = "dashboard.html";
+  })
+  .catch(e => alert(e.message));
+
 }
