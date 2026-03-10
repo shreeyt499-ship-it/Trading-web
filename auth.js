bd6865833cb2e0ff010c115bb9ea43f6424c35ba
shreeyt499-ship-it <shree.yt.499@gmail.com>
2026-03-10 21:18:42 +0530

    Add login and signup functions for authentication

--- auth.js
+++ auth.js
@@ -1,1 +1,33 @@
+function login(){
 
+const email=document.getElementById("email").value
+const password=document.getElementById("password").value
+
+auth.signInWithEmailAndPassword(email,password)
+
+.then(()=>{
+
+window.location="dashboard.html"
+
+})
+
+.catch(e=>alert(e.message))
+
+}
+
+function signup(){
+
+const email=document.getElementById("email").value
+const password=document.getElementById("password").value
+
+auth.createUserWithEmailAndPassword(email,password)
+
+.then(()=>{
+
+window.location="dashboard.html"
+
+})
+
+.catch(e=>alert(e.message))
+
+}
